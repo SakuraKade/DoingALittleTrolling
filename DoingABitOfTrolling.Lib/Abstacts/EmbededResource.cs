3a53e245a9efff3a75292d05ed2c4d5af8111000
Sakura <SakuraKadeHansen@outlook.com>
2023-08-11 21:57:25 +0200

    Abstacts

---
+++ DoingABitOfTrolling.Lib/Abstacts/EmbededResource.cs
@@ -0,0 +1,22 @@
+﻿using DoingABitOfTrolling.Lib.Interfaces;
+using System;
+using System.Linq;
+
+namespace DoingABitOfTrolling.Lib.Abstacts
+{
+    public abstract class EmbededResource<T> : IResource<T> where T : class, IDisposable
+    {
+        protected abstract Lazy<T> resource { get; init; }
+
+        public void Dispose()
+        {
+            if (resource.IsValueCreated)
+                resource.Value.Dispose();
+        }
+
+        public T GetValue()
+        {
+            return resource.Value;
+        }
+    }
+}
