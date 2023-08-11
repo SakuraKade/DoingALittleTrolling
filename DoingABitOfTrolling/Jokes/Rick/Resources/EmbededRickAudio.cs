c88f4c27ef2ffaf43b5927a5d431a66ea48731fe
Sakura <SakuraKadeHansen@outlook.com>
2023-08-11 21:57:41 +0200

    Resources

---
+++ DoingABitOfTrolling/Jokes/Rick/Resources/EmbededRickAudio.cs
@@ -0,0 +1,14 @@
+﻿using DoingABitOfTrolling.Lib.Abstacts;
+
+namespace DoingABitOfTrolling.Jokes.Rick.Resources
+{
+    public class EmbededRickAudio : EmbededResource<UnmanagedMemoryStream>
+    {
+        public EmbededRickAudio()
+        {
+            resource = new Lazy<UnmanagedMemoryStream>(EmbeddedRickAudioResource.rickroll_but_it_never_starts);
+        }
+
+        protected override Lazy<UnmanagedMemoryStream> resource { get; init; }
+    }
+}
